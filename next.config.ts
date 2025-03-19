74ef8339b82f314e26bb42123107b4d4d5ee5979
Yazan Kittaneh <yazanakittaneh@gmail.com>
2025-03-19 14:22:27 -0500

    config change

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,15 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   experimental: {
     ppr: true
-  }
+  },
+  images: {
+    localPatterns: [
+      {
+        pathname: 'images/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
